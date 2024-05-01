7d514427b558a4a848586f2949b390db81ebda5a
Wes1eyCh0i <wesleyc0830lc@gmail.com>
2024-04-30 23:46:02 -0400

    added new 3rd party

---
+++ test.py
@@ -0,0 +1,57 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+web_url = 'http://localhost:8083/login.html'
+domain = "http://localhost:8083/"
+output_file = 'third_party_libs.txt'
+
+
+def extract_urls(long_string):
+    # Define the regular expression pattern
+    pattern = r'\b(?:https?://)\S+\b'
+
+    # Find all matches in the long string
+    matches = re.findall(pattern, long_string)
+
+    return matches
+
+
+def fetch_js_urls(url):
+    # send http
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content)
+    l = [i.get('src') for i in soup.find_all('script') if i.get('src')]
+    return l
+
+
+def combine_domain(endpoints):
+    for i in range(len(endpoints)):
+        if "http" not in endpoints[i] or "https" not in endpoints[i]:
+            endpoints[i] = domain + endpoints[i]
+            endpoints[i] = endpoints[i] .replace('//', '/')
+            endpoints[i] = endpoints[i] .replace('/', '//', 1)
+    return endpoints
+
+
+def fetch_api_urls(urls):
+    result = []
+    # send http
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content)
+        soup_string = str(soup)
+        result = result + extract_urls(soup_string)
+    return result
+
+
+def save_urls_to_file(urls, filename):
+    # write to txt
+    with open(filename, 'w') as file:
+        for url in urls:
+            file.write(url + '\n')
+
+
+urls = (fetch_js_urls(web_url))
+third_parties = (fetch_api_urls(combine_domain(urls)))
+save_urls_to_file(third_parties, output_file)
